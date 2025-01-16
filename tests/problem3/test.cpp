d06eb486f2598f58e9803b3de2d3adc184cb7282
PierrickRauby <pierrick.rauby@gadz.org>
2025-01-16 17:04:21 -0500

    adding solution for problem3 to test skipping logic

---
+++ tests/problem3/test.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include "../main.h"
+#include <string>
+
+TEST(lengthOfLongestSubstring, BasicTest0) {
+    // Inputs
+    std::string input="abcabcbb";
+    //Sol
+    int expectedRes=3;
+    Solution sol;
+    int res=sol.lengthOfLongestSubstring(input);
+    EXPECT_EQ(res, expectedRes);
+}
+TEST(lengthOfLongestSubstring, BasicTest1) {
+    // Inputs
+    std::string input="bbbbbb";
+    //Sol
+    int expectedRes=1;
+    Solution sol;
+    int res=sol.lengthOfLongestSubstring(input);
+    EXPECT_EQ(res, expectedRes);
+}
+
+TEST(lengthOfLongestSubstring, BasicTest2) {
+    // Inputs
+    std::string input="pwwkew";
+    //Sol
+    int expectedRes=3;
+    Solution sol;
+    int res=sol.lengthOfLongestSubstring(input);
+    EXPECT_EQ(res, expectedRes);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
