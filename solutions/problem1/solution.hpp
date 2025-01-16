7ff48ef78e572d9e065a162027e5f29ef79f0be5
PierrickRauby <pierrick.rauby@gadz.org>
2025-01-16 16:53:50 -0500

    added renamed files

---
+++ solutions/problem1/solution.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+public:
+    std::vector<int> twoSum(std::vector<int> &nums, int target);
+};
+#endif // MAIN_H
---
+++ tests/problem1/test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "solution.hpp"
+#include <vector>
+#include <unordered_map>
+
+TEST(twoSum,BasicTest ) {
+    std::vector<int> nums={2,7,11,15};
+    int target=9;
+    Solution sol;
+    std::vector<int> expectedRes={0,1};
+    std::vector<int> res=sol.twoSum(nums,target);
+    EXPECT_EQ(res,expectedRes);
+}
+
+TEST(twoSum,BasicTest2) {
+    std::vector<int> nums={3,2,4};
+    int target=6;
+    Solution sol;
+    std::vector<int> expectedRes={1,2};
+    std::vector<int> res=sol.twoSum(nums,target);
+    EXPECT_EQ(res,expectedRes);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
