23db9aa8be146f146290424d621baff907205b46
PierrickRauby <pierrick.rauby@gadz.org>
2025-02-05 07:49:44 -0500

    drafting test_builder class and reorganized test folder

---
+++ src/test_builder.cpp
@@ -0,0 +1,21 @@
+#include "test_builder.hpp"
+#include <iostream>
+#include <cstdlib>
+
+bool TestBuilder::buildAll(const std::string& buildDir, int numJobs){
+  std::string buildCommand= "cmake --build " + buildDir;
+  if(numJobs >1){
+    buildCommand+= " --parallel "+std::to_string(numJobs);
+  }
+  std::cout<<"Building all tests with command: "<< buildCommand<<std::endl;
+  return std::system(buildCommand.c_str())==0;
+}
+
+bool TestBuilder::buildTest(const std::string& testName, const std::string& buildDir, int numJobs){
+  std::string buildCommand="cmake --build " + buildDir + " --target "+testName;
+  if(numJobs >1){
+    buildCommand+= " --parallel "+std::to_string(numJobs);
+  }
+  std::cout<<"Building test '"<<testName<<"' with command: "<< buildCommand<<std::endl;
+  return std::system(buildCommand.c_str())==0;
+}
